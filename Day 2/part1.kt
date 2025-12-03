bca74d493839cb622d390d407d142ae877881fb4
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-12-03 15:34:41 +0100

    Day 2 Part 1

---
+++ Day 2/part1.kt
@@ -0,0 +1,33 @@
+import java.io.File
+import java.io.InputStream
+
+// First we splice the ranges and put them in a list 
+
+
+fun main(args: Array<String>) {
+    val inputStream: InputStream = File("input.txt").inputStream()
+        var rangeList : MutableList<String> = mutableListOf()
+        val numsToAdd : MutableList<Long> = mutableListOf()
+        inputStream.bufferedReader().forEachLine {
+            rangeList = it.split(",").toMutableList()
+        }
+    println(rangeList[0])
+        rangeList.forEach { range -> 
+            val start = range.split("-")[0]
+                val end   = range.split("-")[1]
+                //println("$start to $end")
+                for (num in start.toLong()..end.toLong()) {
+                    val str = num.toString()
+                        if (str.length % 2 == 0) {
+                            val middleIndex = str.length / 2
+                                if (str.substring(0, middleIndex) == str.substring(middleIndex)) {
+                                    numsToAdd.add(num) 
+                                    println(str) 
+                                }
+                        }
+                }
+
+        }
+        val result : Long = numsToAdd.sum()
+        println(result)
+}
